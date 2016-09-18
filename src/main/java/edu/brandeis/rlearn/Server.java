90db37ed393f020f20aca30180f9127b09a5bac5
Sofiya Semenova <novasofiya@gmail.com>
2016-09-17 23:26:11 -0400

    NAILED IT!

--- src/main/java/edu/brandeis/rlearn/Server.java
+++ src/main/java/edu/brandeis/rlearn/Server.java
@@ -4,142 +4,153 @@ import java.util.*;
 
 import static spark.Spark.*;
 
-import edu.brandeis.wisedb.*;
 import edu.brandeis.wisedb.aws.VMType;
-import edu.brandeis.wisedb.cost.sla.MaxLatencySLA;
 import spark.template.velocity.*;
 import spark.ModelAndView;
 
 public class Server {
+
+	private static HashMap<String, Session> sessionMap = new HashMap<>();
+	private static Hashtable<Integer, String> templates = new Hashtable<>();
+	private static Map<Integer, Map<VMType, Integer>> latencies = new HashMap<>();
+	private static Map<VMType, Integer> forMachine = new HashMap<>();
+
 	public static void main(String[] args) {
-//		Session session = new Session();
 		webSocket("/bandit", BanditWebSocket.class);
 		staticFiles.location("/assets");
 
+		defineDefaults();
+
 		get("/", (req, res) -> renderFirstPage(req));
 
+		//data input workflow:
 		post("/sendInitialDataS", (req, res) -> sendInitialDataS(req));
 		post("/sendInitialDataR", (req, res) -> sendInitialDataR(req));
+		post("/sendSLA", (req, res) -> sendSLA(req));
 		post("/sendNumQueries", (req, res) -> sendNumQueries(req)); //for slearn
+		post("/sendSLA2", (req, res) -> sendSLA2(req));
+		post("doSLEARN", (req, res) -> doSLEARN(req));
 		init();
+	}
+
+	/* urls */
+	public static String renderFirstPage(spark.Request req) {
+		Session session = new Session();
+		sessionMap.put(req.session().id(), session);
+
+		HashMap model = new HashMap();
+		model.put("next-Step", "initialForm.vm");
+
+		model.put("templates", templates);
+		model.put("latencies", latencies);
+		return renderTemplate(model);
+	}
+
+	public static String sendInitialDataS(spark.Request req) {
+		HashMap model = new HashMap();
+		Session session = getSessionFromMap(req);
+		Hashtable<Integer, String> templatesChosen = templatesChosen(req.queryParams());
+
+		session.setLearnType("S");
+		model.put("templates", templatesChosen);
+		session.addTemplates(templatesChosen);
+		model.put("next-Step", "chooseSLA.vm");
+
+		return renderTemplate(model);
+	}
+
+	public static String sendInitialDataR(spark.Request req) {
+		HashMap model = new HashMap();
+		Session session = getSessionFromMap(req);
+		Hashtable<Integer, String> templatesChosen = templatesChosen(req.queryParams());
+
+		session.setLearnType("R");
+		model.put("templates", templatesChosen(req.queryParams()));
+		session.addTemplates(templatesChosen);
+		model.put("next-Step", "chooseSLA.vm");
+
+		return renderTemplate(model);
+	}
+
+	public static String sendSLA(spark.Request req) {
+		HashMap model = new HashMap();
+		Session session = getSessionFromMap(req);
+
+		session.addSLA1(req.queryParams("type"), req.queryParams("value"));
+
+		if (session.isSLEARN()) {
+			model.put("next-Step", "chooseNumQueries.vm");
+			model.put("templates", session.getTemplates());
+		} else {
+			model.put("next-Step", "doRLEARN.vm");
+		}
+
+		return renderTemplate(model);
+	}
+
+	public static String sendNumQueries(spark.Request req) {
+		HashMap model = new HashMap();
+		Session session = getSessionFromMap(req);
+
+		if (session.isSLEARN()) {
+			model.put("next-step", "chooseSLA2.vm");
+			model.put("SLArecs", session.recommendSLA(latencies, forMachine));
+		}
+		return renderTemplate(model);
+	}
+
+	public static String sendSLA2(spark.Request req) {
+		HashMap model = new HashMap();
+		Session session = getSessionFromMap(req);
+
+		if (session.isSLEARN()) {
+			model.put("next-step", "chooseSLA2.vm");
+			model.put("SLArecs", session.recommendSLA(latencies, forMachine));
+		}
+		return renderTemplate(model);
+	}
+
+	public static String doSLEARN(spark.Request req) {
+		return "";
+	}
+
+	/* helpers */
+	private static Session getSessionFromMap(spark.Request req) {
+		return sessionMap.get(req.session().id());
 	}
 
 	private static String renderTemplate(Map model) {
 		return new VelocityTemplateEngine().render(new ModelAndView(model, "index.vm"));
 	}
 
-	public static String renderFirstPage(spark.Request req) {
-		HashMap model = new HashMap();
-		model.put("initial-Data-Form", "initialForm.vm");
-
-		Hashtable<Integer, String> templates = defineTemplates();
-		Map<Integer, Map<VMType, Integer>> latency = defineLatencies();
-
-		model.put("templates", templates);
-		model.put("latency", latency);
-		return renderTemplate(model);
-	}
-
-	public static Hashtable<Integer, String> defineTemplates() {
-		Hashtable<Integer, String> templates = new Hashtable<>();
-		templates.put(1, "SQL QUERY 1");
-		templates.put(2, "SQL QUERY 2");
-		templates.put(3, "SQL QUERY 3");
-
-		return templates;
-	}
-
-	public static Map<Integer, Map<VMType, Integer>> defineLatencies() {
-		Map<Integer, Map<VMType, Integer>> latency = new HashMap<>();
-		Map<VMType, Integer> forMachine = new HashMap<>();
-		forMachine.put(VMType.T2_SMALL, 20000);
-		latency.put(1, forMachine);
-
-		forMachine = new HashMap<>();
-		forMachine.put(VMType.T2_SMALL, 30000);
-		latency.put(2, forMachine);
-
-		forMachine = new HashMap<>();
-		forMachine.put(VMType.T2_SMALL, 40000);
-		latency.put(3, forMachine);
-
-		return latency;
-	}
-
-	public static String sendInitialDataS(spark.Request req) {
-		HashMap model = new HashMap();
-
-		model.put("templates", templatesChosen(req.queryParams()));
-		model.put("initial-Data-Form", "initialFormFilled.vm");
-		model.put("step-Two", "data-s-display.vm");
-
-		return renderTemplate(model);
-	}
-
-	public static List<String> templatesChosen(Set<String> params) {
-		List<String> templatesChosen = new LinkedList<>();
+	public static Hashtable<Integer, String> templatesChosen(Set<String> params) {
+		Hashtable<Integer, String> templatesChosen = new Hashtable<>();
 		for (String param : params) {
 			if (param.contains("template")) {
-				templatesChosen.add(param.substring(8));
+				templatesChosen.put(Integer.valueOf(param.substring(8)), templates.get(Integer.valueOf(param.substring(8))));
 			}
 		}
 
 		return templatesChosen;
 	}
 
-	public static String sendInitialDataR(spark.Request req) {
-		HashMap model = new HashMap();
+	/* wise-specific stuff */
+	public static void defineDefaults() {
+		forMachine.put(VMType.T2_SMALL, 20000);
+		latencies.put(1, forMachine);
 
-		model.put("templates", templatesChosen(req.queryParams()));
-		model.put("initial-Data-Form", "initialFormFilled.vm");
-		model.put("step-Two", "data-r-display.vm");
+		forMachine = new HashMap<>();
+		forMachine.put(VMType.T2_SMALL, 30000);
+		latencies.put(2, forMachine);
 
-		return renderTemplate(model);
+		forMachine = new HashMap<>();
+		forMachine.put(VMType.T2_SMALL, 40000);
+		latencies.put(3, forMachine);
+
+		templates.put(1, "SQL QUERY 1");
+		templates.put(2, "SQL QUERY 2");
+		templates.put(3, "SQL QUERY 3");
+
 	}
 
-	public static ModelAndView sendNumQueries(spark.Request req) {
-		HashMap model = new HashMap();
-		return new ModelAndView(model, "index.vm");
-	}
-
-//	public static void recommendSLA(Map<VMType, Integer> forMachine) {
-//		Map<Integer, Map<VMType, Integer>> ios = new HashMap<>();
-//		forMachine = new HashMap<>();
-//		forMachine.put(VMType.T2_SMALL, 10);
-//		ios.put(1, forMachine);
-//
-//		forMachine = new HashMap<>();
-//		forMachine.put(VMType.T2_SMALL, 10);
-//		ios.put(2, forMachine);
-//
-//		forMachine = new HashMap<>();
-//		forMachine.put(VMType.T2_SMALL, 10);
-//		ios.put(3, forMachine);
-//
-//		int loosestLatency = startLatency + ((Rincrement * Rn)/2);
-//
-//		WorkloadSpecification wf = new WorkloadSpecification(
-//				latency,
-//				ios,
-//				new VMType[] { VMType.T2_SMALL },
-//				new MaxLatencySLA(loosestLatency, penalty));
-//
-//		List<WiSeDBCachedModel> models = AdaptiveModelingUtils.tightenAndRetrain(wf, Rincrement, Rn, numQueries, numWorkloads);
-//
-//		Map<Integer, Integer> freqs = new HashMap<>();
-//		freqs.put(1, 100);
-//		freqs.put(2, 100);
-//		freqs.put(3, 100);
-//
-//		List<Integer> cost = new LinkedList<Integer>();
-//		for (WiSeDBCachedModel model : models) {
-//			cost.add(CostUtils.getCostForPlan(model.getWorkloadSpecification(),
-//					WiSeDBUtils.doPlacement(model, freqs)));
-//		}
-//
-//		List<Integer> minimizedList = minimizeList(cost, numSLAToRecommend);
-//
-//		Map<String, Object> model = new HashMap<>();
-//		model.put("SLARecs", minimizedList);
-//	}
 }
--- src/main/java/edu/brandeis/rlearn/Session.java
+++ src/main/java/edu/brandeis/rlearn/Session.java
@@ -1,11 +1,115 @@
 package edu.brandeis.rlearn;
 
+import edu.brandeis.wisedb.*;
+import edu.brandeis.wisedb.aws.VMType;
+import edu.brandeis.wisedb.cost.sla.MaxLatencySLA;
+
+import java.util.*;
+
 /**
  * Created by seaurchi on 9/17/16.
  */
 public class Session {
+    private Hashtable<Integer, String> templates;
+    private String learnType;
+    private String SLAtype;
+    private final int Rn = 10; // num tightens
+    private final int Rincrement = 5000;
+    private final int numQueries = 9;
+    private final int numWorkloads = 250;
+    private int startLatency;
+    private final int penalty = 1; //penalty for initial SLA
+    private final int numSLAToRecommend = 3; // num of SLAs to recommend
+
 
     public Session() {
-
+        templates = new Hashtable<>();
     }
+
+    public void addTemplates(Hashtable<Integer, String> templates) {
+        this.templates = templates;
+    }
+
+    public Hashtable<Integer, String> getTemplates() {
+        return templates;
+    }
+
+    public void setLearnType(String learnType) {
+        this.learnType = learnType;
+    }
+
+    public Boolean isSLEARN() {
+        return learnType.equals("S");
+    }
+
+    public void addSLA1(String type, String value) {
+        SLAtype = type;
+        startLatency = Integer.parseInt(value);
+    }
+
+    public List<Integer> recommendSLA(Map<Integer, Map<VMType, Integer>> latency, Map<VMType, Integer> forMachine) {
+        Map<Integer, Map<VMType, Integer>> ios = new HashMap<>();
+        forMachine = new HashMap<>();
+        forMachine.put(VMType.T2_SMALL, 10);
+        ios.put(1, forMachine);
+
+        forMachine = new HashMap<>();
+        forMachine.put(VMType.T2_SMALL, 10);
+        ios.put(2, forMachine);
+
+        forMachine = new HashMap<>();
+        forMachine.put(VMType.T2_SMALL, 10);
+        ios.put(3, forMachine);
+
+        int loosestLatency = startLatency + ((Rincrement * Rn)/2);
+
+        WorkloadSpecification wf = new WorkloadSpecification(
+                latency,
+                ios,
+                new VMType[] { VMType.T2_SMALL },
+                new MaxLatencySLA(loosestLatency, penalty));
+
+        List<WiSeDBCachedModel> models = AdaptiveModelingUtils.tightenAndRetrain(wf, Rincrement, Rn, numQueries, numWorkloads);
+
+        Map<Integer, Integer> freqs = new HashMap<>();
+        freqs.put(1, 100);
+        freqs.put(2, 100);
+        freqs.put(3, 100);
+
+        List<Integer> cost = new LinkedList<Integer>();
+        for (WiSeDBCachedModel model : models) {
+            cost.add(CostUtils.getCostForPlan(model.getWorkloadSpecification(),
+                    WiSeDBUtils.doPlacement(model, freqs)));
+        }
+
+        return minimizeList(cost, numSLAToRecommend);
+    }
+
+    private List<Integer> minimizeList(List<Integer> cost, int numToRec) {
+        int[] minPairIndex = new int[2];
+        while (cost.size() > 3) {
+            minPairIndex = findMin(cost);
+            cost.remove(minPairIndex[1]); //remove the first one
+        }
+        return cost;
+    }
+
+    private int[] findMin(List<Integer> cost) {
+        int minDiff = Math.abs(cost.get(0) - cost.get(1));
+        int index1 = 0;
+        int index2 = 1;
+        int curr = 0;
+
+        for (int i = 1; i < cost.size()-1; i++) {
+            curr = Math.abs(cost.get(i) - cost.get(i+1));
+            if (curr < minDiff) {
+                minDiff = curr;
+                index1 = i;
+                index2 = i + 1;
+            }
+        }
+        return new int[]{index1, index2};
+    }
+
+
 }
